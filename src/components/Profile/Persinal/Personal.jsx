be241862108fcd490c4e991a9743698d8228a95f
Anastasia Kutina <kutina223311@gmail.com>
2022-05-31 19:04:36 +0300

    Изменения на 85 урок

--- src/components/Profile/Persinal/Personal.jsx
+++ src/components/Profile/Persinal/Personal.jsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { ProfileStatus } from "../ProfileStatus";
+import { ProfileStatusWithHooks } from "../ProfileStatusWithHooks";
 import s from "./Personal.module.css";
 
 const Personal = (props) => {
   return (
     <div>
       <div className={s.name}>{props.profile.fullName}</div>
-      <ProfileStatus status={props.status} updateStatus={props.updateStatus} />
+      <ProfileStatusWithHooks
+        status={props.status}
+        updateStatus={props.updateStatus}
+      />
       <div>{props.profile.contacts.vk}</div>
       <div>{props.profile.contacts.instagram}</div>
     </div>
